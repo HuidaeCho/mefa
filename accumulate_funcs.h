3598b1742bd90ca28433be4d2cb0930371c35a80
Huidae Cho <grass4u@gmail.com>
2025-02-05 23:55:07 -0700

    Drop casting for DIR_NULL

--- accumulate_funcs.h
+++ accumulate_funcs.h
@@ -3,7 +3,7 @@
 #include "global.h"
 
 #define INDEX(row, col) ((size_t)(row) * ncols + (col))
-#define DIR_NULL (unsigned char)dir_map->null_value
+#define DIR_NULL dir_map->null_value
 #define DIR(row, col) dir_map->cells.byte[INDEX(row, col)]
 #define ACCUM(row, col) accum_map->cells.uint32[INDEX(row, col)]
 #define FIND_UP(row, col) ( \
